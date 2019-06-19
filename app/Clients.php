8c4bd43e8d07f4baf73cf982955b6c67c2723b38
Shaun.Sproston <shaun.sproston@bluebaytravel.co.uk>
2019-06-19 11:50:58 +0100

    changed fillable, formatting and field value

--- app/Clients.php
+++ app/Clients.php
@@ -2,38 +2,16 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 
-class Clients extends Authenticatable
+class Clients extends Model
 {
-    use Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
-     * @var array
+     * @var string[]
      */
     protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
+        'name',
     ];
 }
