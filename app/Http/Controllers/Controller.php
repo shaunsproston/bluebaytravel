4f65e61f214373bd86232047cb197e3ef0860b09
Shaun.Sproston <shaun.sproston@bluebaytravel.co.uk>
2019-06-26 15:07:50 +0100

    Created API routes and resources

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,9 +6,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use App\Client;
-use App\Treatment;
-use App\Appointment;
+
 
 
 class Controller extends BaseController
@@ -17,8 +15,7 @@ class Controller extends BaseController
 
     public function index()
     {   
-        return view('data')
-            ->withAppointments(Appointment::all()->load(['client', 'treatment']));
+        return view('data');
     }
 
 }
